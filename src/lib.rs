0a8d493f835b7e5ff1c317681f6aa68eb7e9d6f5
Xinyu <Pana.Yang@hotmail.com>
2026-01-17 19:32:29 +0800

    Style: add clippy llints

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,52 @@
 #![doc(
     html_favicon_url = "https://raw.githubusercontent.com/Apich-Organization/rssn/refs/heads/dev/doc/favicon.ico"
 )]
+// -------------------------------------------------------------------------
+// Rust Lint Configuration: rssn-advanced
+// -------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------
+// LEVEL 1: CRITICAL ERRORS (Deny)
+// -------------------------------------------------------------------------
+#![deny(
+    // Rust Compiler Errors
+    dead_code,
+    unreachable_code,
+    improper_ctypes_definitions,
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_idioms,
+    clippy::perf,
+    clippy::correctness,
+    clippy::suspicious,
+    clippy::unwrap_used,
+    clippy::expect_used,
+    clippy::indexing_slicing,
+    clippy::arithmetic_side_effects,
+    clippy::missing_safety_doc,
+    clippy::same_item_push,
+    clippy::implicit_clone,
+    clippy::all,
+    clippy::pedantic,
+    warnings,
+    missing_docs,
+    clippy::nursery,
+    clippy::single_call_fn,
+)]
+// -------------------------------------------------------------------------
+// LEVEL 2: STYLE WARNINGS (Warn)
+// -------------------------------------------------------------------------
+#![warn(
+    unsafe_code,
+    clippy::dbg_macro,
+    clippy::todo,
+    clippy::unnecessary_safety_comment
+)]
+// -------------------------------------------------------------------------
+// LEVEL 3: ALLOW/IGNORABLE (Allow)
+// -------------------------------------------------------------------------
+#![allow(
+    clippy::restriction,
+    unused_doc_comments,
+    clippy::empty_line_after_doc_comments
+)]
